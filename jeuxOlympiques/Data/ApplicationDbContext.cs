1e1c303b13ea6c8589fda41585385c14f65e1ff5
Renaud Ciesielski <renaudciesielski@gmail.com>
2024-04-23 17:49:13 +0200

    configuration connection BDD juste avant création BDD 1h32

---
+++ jeuxOlympiques/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace jeuxOlympiques.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+    }
+}
