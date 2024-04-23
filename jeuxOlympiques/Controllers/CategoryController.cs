0807b93ff83d59b3234bf7656192370e74bd6228
RenaudCiesielski <139341426+RenaudCiesielski@users.noreply.github.com>
2024-04-23 21:38:20 +0200

    création vue et onglet "Catégories"

---
+++ jeuxOlympiques/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace jeuxOlympiques.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
