bf787720af98c8dc062dbe790e0c15017aa83be2
RenaudCiesielski <139341426+RenaudCiesielski@users.noreply.github.com>
2024-04-25 22:20:38 +0200

    mise en place du lien du form vers la base de données

--- jeuxOlympiques/Models/Category.cs
+++ jeuxOlympiques/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace jeuxOlympiques.Models
 {
@@ -6,9 +7,20 @@ namespace jeuxOlympiques.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
+        [DisplayName("Catégorie Ticket")]
+        [MinLength(3)]
         public string Name { get; set; }
+
+
+        [DisplayName("Description")]
+        [MinLength(10)]
         public string Description { get; set; }
+
+
+        [DisplayName("Prix")]
+        [MinLength(1)]
         public int Price { get; set; }
     }
 }
